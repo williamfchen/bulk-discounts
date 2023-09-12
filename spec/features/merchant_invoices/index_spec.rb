ce046e878ee57a22fddcd0ada0adbcd4fb023c62
Will Chen <wfchen1990@gmail.com>
2023-09-11 22:38:27 -0600

    Start US14 testing

--- spec/features/merchant_invoices/index_spec.rb
+++ spec/features/merchant_invoices/index_spec.rb
@@ -2,13 +2,16 @@ require "rails_helper"
 
 RSpec.feature "the merchant invoices index page" do
   describe 'when visiting /merchants/merchant_id/invoices' do
-    it 'US14 displays a list of merchant invoices with a button to each invoice show page' do
+    xit 'US14 displays a list of merchant invoices with a button to each invoice show page' do
       merchant = Merchant.create!(name: "Bracelets 'n Stuff")
-      customer = Customer.create!(first_name: "Robert", last_name: "Redford")
-      invoice_1 = merchant.invoices.create!(customer_id: customer.id, status: 0)
+      item = merchant.items.create!(name: "Bracelet", description: "Shiny", unit_price: 100)
+      
 
       visit "/merchants/#{merchant.id}/invoices"
 
       expect(page).to have_button(invoice_1.id)
+
+      expect(button(invoice_1.id)).to have_path "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+    end
   end
 end

b9ec604ec148726ab0301495cbf971f5ca969859
Antoine Aube <aaube3@gmail.com>
2023-09-12 17:42:12 -0600

    test: feature test merchant items show page

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,31 +1,34 @@
 require "rails_helper"
 
 RSpec.feature "the merchant items index page" do
+    before :each do
+      @merchant_1 = Merchant.create!(name: "Bracelets 'n Stuff")
+      @merchant_2 = Merchant.create!(name: "Necklaces 'n Stuff")
+      @item_1 = @merchant_1.items.create!(name: "Bracelet", description: "Shiny", unit_price: 1000)
+      @item_2 = @merchant_1.items.create!(name: "Necklace", description: "Shiny", unit_price: 2000)
+      @item_3 = @merchant_2.items.create!(name: "Ring", description: "Shiny", unit_price: 3000)
+    end 
+    
   describe 'when visiting /merchants/merchant_id/items' do
     it 'US6 displays a list of the merchants items' do
-      merchant_1 = Merchant.create!(name: "Bracelets 'n Stuff")
-      merchant_2 = Merchant.create!(name: "Necklaces 'n Stuff")
-      item_1 = merchant_1.items.create!(name: "Bracelet", description: "Shiny", unit_price: 1000)
-      item_2 = merchant_1.items.create!(name: "Necklace", description: "Shiny", unit_price: 2000)
-      item_3 = merchant_2.items.create!(name: "Ring", description: "Shiny", unit_price: 3000)
 
-      visit "/merchants/#{merchant_1.id}/items"
+      visit "/merchants/#{@merchant_1.id}/items"
 
-      expect(page).to have_content(item_1.name)
-      expect(page).to have_content(item_2.name)
-      expect(page).to_not have_content(item_3.name)
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_2.name)
+      expect(page).to_not have_content(@item_3.name)
     end
 
     it "US7 item has a link to go to an items show page" do 
-      visit "/merchants/#{merchant_1.id}/items"
+      visit "/merchants/#{@merchant_1.id}/items"
 
-      click_link "#{item_1.name}"
+      click_link "#{@item_1.name}"
 
-      expect(current_path).to eq("/merchants/#{merchant_1.id}/items/#{item_1.id}")
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
 
-      expect(page).to have_content(item_1.name)
-      expect(page).to have_content(item_1.description)
-      expect(page).to have_content(item_1.unit_price)
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_1.description)
+      expect(page).to have_content(@item_1.unit_price)
     end
   end
 end

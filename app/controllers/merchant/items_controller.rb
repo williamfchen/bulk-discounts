0d5a24dacaaf2ffdcdc8264ab7f4b24cdc9e07fb
Antoine Aube <aaube3@gmail.com>
2023-09-12 12:05:35 -0600

    refactor file structure for merchants controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,4 +1,4 @@
-class MerchantItemsController < ApplicationController
+class Merchant::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
   get "/merchants/:merchant_id/dashboard", to: "merchants#show"
-  get "/merchants/:merchant_id/items", to: "merchant_items#index"
-  get "/merchants/:merchant_id/invoices", to: "merchant_invoices#index"
-  get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant_invoices#show", as: :merchant_invoice
+  get "/merchants/:merchant_id/items", to: "merchant/items#index"
+  get "/merchants/:merchant_id/invoices", to: "merchant/invoices#index"
+  get "/merchants/:merchant_id/invoices/:invoice_id", to: "merchant/invoices#show", as: :merchant_invoice
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # namespace :merchant do
+  
+  #     resources :invoices, only: [:index, :show]
+  #     resources :items, only: [:index]
+
+  # end
+
+  # resources :merchants, only: [:show]
+
+  # resources :merchants, param: :merchant_id do
+  #   get "dashboard", on: :member, to: "merchants#show_dashboard"
+  #   resources :items, only: [:index]
+  #   resources :invoices, only: [:index, :show]
+  # end
 end

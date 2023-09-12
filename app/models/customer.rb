b334c04c5e9857826b902d29bbaa3dd71b09ffbc
Antoine Aube <aaube3@gmail.com>
2023-09-11 21:23:06 -0600

    models: add relationships to customer, invoice, and item

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
+  #would this one beloew need to be through through(through items, through invoice_items)? Know what I mean? 
+  # has_many :items, through: :invoices
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many items, through: :invoice_items
 
   enum :status,["in progress", "completed", "cancelled"]
 

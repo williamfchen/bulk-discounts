21c34b536c154fbc072352c261d554a83de82bb4
Will Chen <wfchen1990@gmail.com>
2023-09-11 22:48:49 -0600

    Add US14 test

--- spec/features/merchant_invoices/index_spec.rb
+++ spec/features/merchant_invoices/index_spec.rb
@@ -2,16 +2,18 @@ require "rails_helper"
 
 RSpec.feature "the merchant invoices index page" do
   describe 'when visiting /merchants/merchant_id/invoices' do
-    xit 'US14 displays a list of merchant invoices with a button to each invoice show page' do
+    it 'US14 displays a list of merchant invoices with a button to each invoice show page' do
       merchant = Merchant.create!(name: "Bracelets 'n Stuff")
       item = merchant.items.create!(name: "Bracelet", description: "Shiny", unit_price: 100)
-      
+      customer = Customer.create!(first_name: "Robert", last_name: "Redford")
+      invoice = customer.invoices.create!(status: 0)
+      invoice_item = InvoiceItem.create!(item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: 100, status: 0)
 
       visit "/merchants/#{merchant.id}/invoices"
 
-      expect(page).to have_button(invoice_1.id)
-
-      expect(button(invoice_1.id)).to have_path "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+      expect(page).to have_button(invoice.id)
+      click_button(invoice.id)
+      expect(page).to have_current_path("/merchants/#{merchant.id}/invoices/#{invoice.id}")
     end
   end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "the merchant show/dashbaord page" do
+RSpec.feature "the merchant show/dashboard page" do
   describe 'when visiting /merchants/merchant_id/dashboard' do
     it 'US1 shows the merchant name' do
       merchant = Merchant.create(name: "Bracelets 'n Stuff")
@@ -20,7 +20,7 @@ RSpec.feature "the merchant show/dashbaord page" do
       expect(page).to have_current_path("/merchants/#{merchant.id}/items")
 
       visit "/merchants/#{merchant.id}/dashboard"
-      
+
       expect(page).to have_button("View Invoices")
       click_button("View Invoices")
       expect(page).to have_current_path("/merchants/#{merchant.id}/invoices")

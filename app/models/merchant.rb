925f9dc3ac8ee37d1a49b75aed95d8dc2ada0502
Antoine Aube <aaube3@gmail.com>
2023-09-13 15:29:16 -0600

    model: top_5_customers query

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,17 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices, through: :items
-  
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
   validates :name, presence: true
+
+  def top_five_customers
+    self.customers
+      .select('customers.*, count(transactions.*) AS transaction_count')
+      .where('transactions.result = 1')
+      .joins(invoices: :transactions)
+      .group('customers.id')
+      .order(transaction_count: :desc)
+      .limit(5)
+  end
 end

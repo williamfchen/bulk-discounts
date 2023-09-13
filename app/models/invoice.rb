f7deeb92c013063f61d35ae4a0e3c606b2552dfc
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-12 22:25:49 -0500

    feat: added features and testing for showing incomplete invoices on the admin page with link

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,14 @@ class Invoice < ApplicationRecord
   validates :status, presence: true
 
   enum :status,["in progress", "completed", "cancelled"]
+
+  def self.incomplete_invoices
+    Invoice
+    .joins(:invoice_items)
+    .select('invoices.*')
+    .where.not('invoice_items.status = ?', 2)
+    .group('invoices.id')
+    .order(:id)
+
+  end
 end

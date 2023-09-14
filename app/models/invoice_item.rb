65485c9601d2800d8f80dc2bbff79607fe06ba07
Will Chen <wfchen1990@gmail.com>
2023-09-14 09:53:07 -0600

    Comment out bugged, unncessary invoice item quantity and pricing

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,4 +8,18 @@ class InvoiceItem < ApplicationRecord
   validates :status, presence: true
 
   enum :status,["packaged", "pending", "shipped"]
+
+  # def self.successful_average_item_price
+  #   InvoiceItem
+  #     .joins(invoices: :transactions)
+  #     .select('items.*, average(invoice_items.unit_price) as price')
+  #     .where('transactions.result = ?', 1)
+  # end
+
+  # def self.successful_total_item_quantity
+  #   InvoiceItem
+  #     .joins(invoices: :transactions)
+  #     .select('items.*, average(invoice_items.unit_price) as price')
+  #     .where('transactions.result = ?', 1)
+  # end
 end

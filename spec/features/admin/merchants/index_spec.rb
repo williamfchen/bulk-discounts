8a6e97e38aabe37b2c3c926a5bf620d8dcf9aeca
pcbennett108 <pcbennett42@gmail.com>
2023-09-15 14:34:35 -0600

    Add test blocks for top_5_merchants and test for desc order by revenue

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,4 +1,6 @@
 require "rails_helper"
+include ActionView::Helpers::NumberHelper
+include ApplicationHelper
 
 RSpec.feature "the admin/merchants index page" do
   before(:each) do
@@ -206,12 +208,38 @@ RSpec.feature "the admin/merchants index page" do
       expect(page).to have_content("Top Five Merchants By Total Revenue")
 
       within("#top_five") do
-        within("#merchant-#{@merchant_1.id}") do
-          expect(page).to have_link("#{@merchant_1.name}", href: admin_merchant_path(@merchant_1))
-          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_1.total_revenue)}")
+        within("#merchant-#{@merchant_6.id}") do
+          expect(page).to have_link("#{@merchant_6.name}", href: admin_merchant_path(@merchant_6))
+          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_6.total_revenue)}")
         end
+  
+        within("#merchant-#{@merchant_2.id}") do
+          expect(page).to have_link("#{@merchant_2.name}", href: admin_merchant_path(@merchant_2))
+          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_2.total_revenue)}")
+        end
+  
+        within("#merchant-#{@merchant_4.id}") do
+          expect(page).to have_link("#{@merchant_4.name}", href: admin_merchant_path(@merchant_4))
+          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_4.total_revenue)}")
+        end 
+  
+        within("#merchant-#{@merchant_5.id}") do
+          expect(page).to have_link("#{@merchant_5.name}", href: admin_merchant_path(@merchant_5))
+          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_5.total_revenue)}")
+        end
+        
+        within("#merchant-#{@merchant_7.id}") do
+          expect(page).to have_link("#{@merchant_7.name}", href: admin_merchant_path(@merchant_7))
+          expect(page).to have_content("Total Revenue: #{number_to_currency(@merchant_7.total_revenue)}")
+        end
+  
+        expect(@merchant_6.name).to appear_before(@merchant_2.name)
+        expect(@merchant_2.name).to appear_before(@merchant_4.name)
+        expect(@merchant_4.name).to appear_before(@merchant_5.name)
+        expect(@merchant_5.name).to appear_before(@merchant_7.name)
       end
 
     end
   end
 end
+#number_to_currency(@merchant_6.total_revenue)

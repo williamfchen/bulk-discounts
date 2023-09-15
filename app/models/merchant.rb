b03c8b1bca0d6c8468eed8f4a31707469bdd0092
Antoine Aube <aaube3@gmail.com>
2023-09-15 16:10:19 -0600

    fix: bug in pathing for invoice id in merchants main index view

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -34,9 +34,9 @@ class Merchant < ApplicationRecord
 
   def items_ready_to_ship
     items.joins(:invoices)
-    .select('invoices.*, items.*, invoice_items.invoice_id AS invoice_id, invoices.created_at AS invoice_created_at')
+    .select('invoices.*, items.*, invoices.id AS invoice_id, invoices.created_at AS invoice_created_at')
     .where.not('invoice_items.status = ?', 2)
-    .group('invoices.id, items.id, invoice_items.invoice_id, invoices.created_at')
+    .group('invoices.id, items.id, invoices.created_at')
     .order('invoices.created_at')
   end
 end

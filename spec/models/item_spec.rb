3179984a319730e0796372d5c9707bbde08c8257
Antoine Aube <aaube3@gmail.com>
2023-09-18 16:14:35 -0600

    refactor: get rid for form_for, delete old tests, clean up comments

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
-    # it { should validate_presence_of :status }
+    it { should validate_presence_of :status }
   end
 
   describe instance_methods do 
